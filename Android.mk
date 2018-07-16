ab0298212142597a260761e6bb975c8d0dcdd528
Chuanzhi Wang <chuanzhi.wang@amlogic.com>
2018-07-16 15:47:45 +0800

    libzvbi: fixed compile error for N [1\1]
    PD#none
    
    Change-Id: I9fe46ff03ccf6c642967b6daad160e0dd70eaa43

--- Android.mk
+++ Android.mk
@@ -18,8 +18,7 @@ endif
 LOCAL_ARM_MODE := arm
 LOCAL_C_INCLUDES := external/icu4c/common
 LOCAL_C_INCLUDES := vendor/amlogic/dvb/include/am_adp
-LOCAL_C_INCLUDES := vendor/amlogic/external/dvb/include/am_adp
-#LOCAL_C_INCLUDES := external/dvb/include/am_adp
+LOCAL_C_INCLUDES += vendor/amlogic/external/dvb/include/am_adp
 LOCAL_C_INCLUDES += external/icu/icu4c/source/common
 
 LOCAL_SHARED_LIBRARIES += libicuuc liblog libam_adp
